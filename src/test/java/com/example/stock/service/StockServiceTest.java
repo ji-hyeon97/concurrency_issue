f7a7a34484272f21e849f1652c2a66cfa85cc241
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2023-10-06 15:07:15 +0900

    Pessimistic Lock : 데이터 접근 하기 전에 락을 먼저 걸어서 충동 예방

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class StockServiceTest {
 
     @Autowired
-    private StockService stockService;
+    private PessimisticLockStockService stockService;
 
     @Autowired
     private StockRepository stockRepository;
