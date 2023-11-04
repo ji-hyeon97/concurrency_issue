e98974507344ad31b2477bf2997601584609996f
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2023-11-05 00:41:01 +0900

    NamedLock : metadata locking 방법을 활용한 동시성 문제 해결

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,8 +15,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    //@Transactional
-    public synchronized void decrease(Long id, Long quantity){
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void decrease(Long id, Long quantity){
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
 
